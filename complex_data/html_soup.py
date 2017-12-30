4d8f274dd90cc5f0ba8944adacda95b7359d78f1
sian-nadin <nadin.sian@gmail.com>
2017-12-30 21:41:51 +0000

    add data in more complex forms notes

---
+++ complex_data/html_soup.py
@@ -0,0 +1,56 @@
+
+# Your task is to process the HTML using BeautifulSoup, extract the hidden
+# form field values for "__EVENTVALIDATION" and "__VIEWSTATE" and set the appropriate
+# values in the data dictionary.
+# All your changes should be in the 'extract_data' function
+
+# Note that the second argument to the BeautifulSoup function, "lxml", comes from
+# the parser in the Python library 'lxml'. Other parsers can be set up as the second
+# argument, such as the Python library's default "html.parser"
+
+from bs4 import BeautifulSoup
+import requests
+import json
+
+html_page = "page_source.html"
+
+
+def extract_data(page):
+    data = {"eventvalidation": "",
+            "viewstate": ""}
+    with open(page, "r") as html:
+        soup = BeautifulSoup(html, "lxml")
+        ev = soup.find(id="__EVENTVALIDATION")
+        data["eventvalidation"] = ev["value"]
+
+        vs = soup.find(id="__VIEWSTATE")
+        data["viewstate"] = vs["value"]
+
+    return data
+
+
+def make_request(data):
+    eventvalidation = data["eventvalidation"]
+    viewstate = data["viewstate"]
+
+    r = requests.post("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+                    data={'AirportList': "BOS",
+                          'CarrierList': "VX",
+                          'Submit': 'Submit',
+                          "__EVENTTARGET": "",
+                          "__EVENTARGUMENT": "",
+                          "__EVENTVALIDATION": eventvalidation,
+                          "__VIEWSTATE": viewstate
+                    })
+
+    return r.text
+
+
+def test():
+    data = extract_data(html_page)
+    assert data["eventvalidation"] != ""
+    assert data["eventvalidation"].startswith("/wEWjAkCoIj1ng0")
+    assert data["viewstate"].startswith("/wEPDwUKLTI")
+
+
+test()
